35c2f2fdb6a516614356ce976629e9219709ba4e
KuteChica <bridgettossou13@gmail.com>
2026-08-20 12:57:10 +0000

    set folder structure

---
+++ move_mate/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+function Home(){
+  return(
+    <div>
+      <h1>Welcome to Move Mate</h1>
+     
+      <p>Your smart campus shuttle companion</p>
+    </div>
+
+  );
+}
+
+export default Home;
---
+++ move_mate/src/pages/Signup/Signup.jsx
@@ -0,0 +1,9 @@
+function Signup() {
+  return (
+    <div>
+      <h1>Create an Account</h1>
+    </div>
+  );
+}
+
+export default Signup;
