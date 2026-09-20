09e36dd735cbcbb34b0635c167d7b9ac28c9c3cf
BridgetTossou <bftossou@st.ug.edu.gh>
2026-09-20 01:54:55 +0000

    Fix Swagger hosted server URL

--- move_mate/movemate_backend/lib/swagger.js
+++ move_mate/movemate_backend/lib/swagger.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const swaggerJSDoc = require("swagger-jsdoc");
-const config = require("../config");
 
 const options = {
   definition: {
@@ -12,8 +11,8 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${config.server.port}`,
-        description: "Local development server"
+        url: "/",
+        description: "Current MoveMate API server"
       }
     ],
     components: {
